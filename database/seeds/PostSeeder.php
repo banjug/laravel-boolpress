3f5ba2114c8fc49823170ddb8ca9aa4f3ca70efb
Samuele Miotto <miotto.samuele@gmail.com>
2022-02-08 14:58:49 +0100

    first commit

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use App\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Post::class,50)->make()->each(function($post) {
+            $category = Category::inRandomOrder()->limit(1)->first();
+            $post -> category()->associate($category);
+            $post -> save();
+        });
+    }
+}
